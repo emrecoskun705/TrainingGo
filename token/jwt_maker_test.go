a6388e9e1cc5c18723e860236de1f23cacd2244f
emrecoskun705 <emrecoskun705@gmail.com>
2023-10-09 21:51:44 +0300

    add jwt and test

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,32 @@
+package token
+
+import (
+	"github.com/emrecoskun705/TraniningGo/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute * 10
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Subject)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt.Time, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiresAt.Time, time.Second)
+}
---
+++ token/payload.go
@@ -0,0 +1,39 @@
+package token
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+)
+
+type Payload struct {
+	jwt.RegisteredClaims
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	claims := jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		NotBefore: jwt.NewNumericDate(time.Now()),
+		Issuer:    "test",
+		Subject:   username,
+		ID:        tokenID.String(),
+		Audience:  []string{"somebody_else"},
+	}
+
+	payload := &Payload{
+		claims,
+	}
+
+	return payload, nil
+}
