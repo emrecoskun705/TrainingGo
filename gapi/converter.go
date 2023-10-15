526619ffebe50f23dde4a0cebed01872081393e9
emrecoskun705 <emrecoskun705@gmail.com>
2023-10-15 14:39:09 +0300

    add gRPC gateway

---
+++ gapi/converter.go
@@ -0,0 +1,17 @@
+package gapi
+
+import (
+	db "github.com/emrecoskun705/TraniningGo/db/sqlc"
+	"github.com/emrecoskun705/TraniningGo/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,74 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+	db "github.com/emrecoskun705/TraniningGo/db/sqlc"
+	"github.com/emrecoskun705/TraniningGo/pb"
+	"github.com/emrecoskun705/TraniningGo/util"
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := server.store.GetUser(ctx, req.GetUsername())
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to find user")
+	}
+
+	err = util.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "incorrect password")
+	}
+
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create access token")
+	}
+
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.RefreshTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create refresh token")
+	}
+
+	parsedUUID, err := uuid.Parse(refreshPayload.ID)
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "invalid uuid")
+	}
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           parsedUUID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    "",
+		ClientIp:     "",
+		IsBlocked:    false,
+		ExpiresAt:    refreshPayload.ExpiresAt.Time,
+	})
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create session")
+	}
+
+	rsp := &pb.LoginUserResponse{
+		User:                  convertUser(user),
+		SessionId:             session.ID.String(),
+		AccessToken:           accessToken,
+		RefreshToken:          refreshToken,
+		AccessTokenExpiresAt:  timestamppb.New(accessPayload.ExpiresAt.UTC()),
+		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiresAt.UTC()),
+	}
+
+	return rsp, nil
+}
