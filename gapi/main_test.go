76e9e97d02b16fd67a1caf04d09ed4bc1a80d938
emrecoskun705 <emrecoskun705@gmail.com>
2023-10-21 00:42:50 +0300

    add update test that requires authorization(access token)

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,10 +1,14 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	db "github.com/emrecoskun705/TraniningGo/db/sqlc"
+	"github.com/emrecoskun705/TraniningGo/token"
 	"github.com/emrecoskun705/TraniningGo/util"
 	"github.com/emrecoskun705/TraniningGo/worker"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 	"testing"
 	"time"
 )
@@ -19,4 +23,16 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithErrorToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
